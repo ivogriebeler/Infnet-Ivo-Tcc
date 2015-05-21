0bcb773ec51d0c2de165593965cc9c65b9feb7b5
ivogriebeler <ivogriebeler@yahoo.com.br>
2015-05-21 20:04:06 -0300

    Criação dos repositórios.

---
+++ Infnet.Ivo.Tcc.Domain/Repositories/ISerieRepository.cs
@@ -0,0 +1,13 @@
+﻿using Infnet.Ivo.Tcc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infnet.Ivo.Tcc.Domain.Repositories
+{
+    public interface ISerieRepository : IRepository<Serie, Guid>
+    {
+    }
+}
