b24f78079be7e70e995d91112f9870f60014435f
ivogriebeler <ivogriebeler@yahoo.com.br>
2015-05-21 22:53:34 -0300

    Inicio da camada de Servico.

---
+++ Infnet.Ivo.Tcc.Service/Implementations/ServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using Infnet.Ivo.Tcc.Domain.UnitOfWork;
+using Infnet.Ivo.Tcc.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infnet.Ivo.Tcc.Service.Implementations
+{
+    public class ServiceBase : IServiceBase
+    {
+        private IUnitOfWork unitOfWork;
+
+        protected IUnitOfWork UnitOfWork
+        {
+            get
+            {
+                return this.unitOfWork;
+            }
+        }
+
+        public ServiceBase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public virtual void Dispose()
+        {
+            this.unitOfWork.Dispose();
+        }
+    }
+}
